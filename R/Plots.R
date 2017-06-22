ea2de7eb343cf79420e7eee8c567c15b6ac26efd
Simon Kucharsky <kucharssim@gmail.com>
2017-06-22 15:13:05 +0200

    Update Plots.R

--- R/Plots.R
+++ R/Plots.R
@@ -1,6 +1,12 @@
 plotSR <- function(values, title=NULL, includeVal = FALSE,
-                   nAOI=sqrt(length(values))){
+                   nAOI=sqrt(length(values)), g=NULL){
   m <- matrix(values, ncol=nAOI)
+  if(g==1) {stop("gamma cannot be equal to 1")
+  if(!is.null(g)) {
+    minmax <- c(0, 1/(1-g))
+  } else {
+    minmax <- c(0, max(m))
+  }
   corrplot(m, method="shade", is.corr = FALSE,
            tl.col = "black", tl.srt = 0, tl.offset = 0.7,
            p.mat=m, insig = ifelse(includeVal, "p-value", "n"),
